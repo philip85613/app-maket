8be2183bee22746529e9fca455f244a0b7025868
zhouzhenyi <535320162@qq.com>
2018-08-28 09:53:42 +0800

    8.28

---
+++ src/com/appinfosys/controller/forAppBox/AppRankingListController.java
@@ -0,0 +1,31 @@
+package com.appinfosys.controller.forAppBox;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.appinfosys.pojo.AppInfo;
+import com.appinfosys.service.appInfo.AppInfoService;
+
+@Service
+@RequestMapping("/rankinglist")
+public class AppRankingListController {
+	@Resource
+	private AppInfoService appInfoService;
+	private Logger log=Logger.getLogger(AppRankingListController.class);
+	@RequestMapping("/appinfolist.json")
+	@ResponseBody
+	public Object getAppInfoList(){
+		log.debug("getAppInfoList>>>>>>>>>>>>>>>");
+		List<AppInfo> appInfoList=null;
+		appInfoList=appInfoService.getAllAppsInfo();
+		log.debug("JSON.toJSON(appInfoList)>>>>>>>>>>"+JSON.toJSON(appInfoList));
+		return JSON.toJSON(appInfoList);
+	}
+}
